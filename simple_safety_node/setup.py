b9236acf756668d0697594ffcfe235dd37cb45cb
JIN-MAIN-LABTOP\jhkim <jhkim07@gmail.com>
2024-07-31 19:14:39 +0900

    update simple_safety_node

--- simple_safety_node/setup.py
+++ simple_safety_node/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-        'simple_safety_node=simple_safety_node.simple_safety_node:main'
+            'simple_safety_node=simple_safety_node.simple_safety_node:main'
         ],
     },
 )
